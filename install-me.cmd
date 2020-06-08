4e12ceec3f51a56825a50d9c88a9e00cf93dea52
Bryto <bryto-dev@gmail.com>
2020-06-07 18:17:00 -0700

    bad path in installer - oopsie!

--- install-me.cmd
+++ install-me.cmd
@@ -6,4 +6,4 @@ MKDIR %APPDATA%\templates\react\src
 COPY .\src\create-react-app-quick.cmd %APPDATA%\npm\
 COPY .\src\templates\react\* %APPDATA%\templates\react\*
 COPY .\src\templates\react\public\* %APPDATA%\templates\react\public\*
-COPY .\src\templates\react\public\src\* %APPDATA%\templates\react\public\src\*
+COPY .\src\templates\react\src\* %APPDATA%\templates\react\src\*
